31be9211226878d31c927c21025b3891d5e6ceba
Franco Correia <francow.correia@gmail.com>
2020-06-12 02:29:22 -0300

    Boton de Play y Yes en Quit funcionales

---
+++ Assets/Scripts/UI/Menus/MainMenuScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    public void BtnPlay(int sceneIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void BtnQuit()
+    {
+        Application.Quit();
+    }
+}

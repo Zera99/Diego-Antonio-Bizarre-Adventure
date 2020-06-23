825a4d0df97491a283be334d7de463c53f37f588
Zera99 <razprog@hotmail.com>
2020-06-23 16:48:05 -0300

    Sparky, FlammableBox, Pedro y Perro sin titileo

--- Assets/Scripts/Enemies/Big Boss/BigBossView.cs
+++ Assets/Scripts/Enemies/Big Boss/BigBossView.cs
@@ -6,12 +6,12 @@ public class BigBossView : MonoBehaviour {
 
     Animator _anim;
     SpriteRenderer _renderer;
-    Color _oldColor;
+    //Color _oldColor;
 
     private void Start() {
         _anim = GetComponent<Animator>();
         _renderer = GetComponent<SpriteRenderer>();
-        _oldColor = _renderer.color;
+        //_oldColor = _renderer.color;
     }
 
     public void SetAttacking(bool state) {
@@ -20,17 +20,17 @@ public class BigBossView : MonoBehaviour {
 
     public void Stun() {
         _anim.SetTrigger("isStunned");
-        StartCoroutine(StunCoroutine());
+        //StartCoroutine(StunCoroutine());
     }
 
-    IEnumerator StunCoroutine() {
-        _renderer.color = new Color(175, 175, 175, 0.7f);
-        for (int i = 0; i < 8; i++) {
-            _renderer.enabled = false;
-            yield return new WaitForSeconds(0.1f);
-            _renderer.enabled = true;
-            yield return new WaitForSeconds(0.05f);
-        }
-        _renderer.color = _oldColor;
-    }
+    //IEnumerator StunCoroutine() {
+    //    _renderer.color = new Color(175, 175, 175, 0.7f);
+    //    for (int i = 0; i < 8; i++) {
+    //        _renderer.enabled = false;
+    //        yield return new WaitForSeconds(0.1f);
+    //        _renderer.enabled = true;
+    //        yield return new WaitForSeconds(0.05f);
+    //    }
+    //    _renderer.color = _oldColor;
+    //}
 }
--- Assets/Scripts/Enemies/Roamer/RoamerView.cs
+++ Assets/Scripts/Enemies/Roamer/RoamerView.cs
@@ -6,12 +6,12 @@ public class RoamerView : MonoBehaviour
 {
     Animator _anim;
     SpriteRenderer _renderer;
-    Color _oldColor;
+    //Color _oldColor;
 
     private void Start() {
         _anim = this.GetComponent<Animator>();
         _renderer = this.GetComponent<SpriteRenderer>();
-        _oldColor = _renderer.color;
+        //_oldColor = _renderer.color;
     }
 
     public void SpriteFlip(bool flip) {
@@ -20,7 +20,7 @@ public class RoamerView : MonoBehaviour
 
     public void TakeDamage() {
         _anim.Play("Damage Animation");
-        StartCoroutine(BlinkEffect());
+        //StartCoroutine(BlinkEffect());
     }
 
     public void Die() {
@@ -31,17 +31,17 @@ public class RoamerView : MonoBehaviour
         _anim.Play("Attack Animation");
     }
 
-    IEnumerator BlinkEffect()
-    {
-        _renderer.color = new Color(255, _oldColor.g/2, _oldColor.b/2, 0.8f);
-        for (int i = 0; i < 5; i++)
-        {
-            _renderer.enabled = false;
-            yield return new WaitForSeconds(0.1f);
-            _renderer.enabled = true;
-            yield return new WaitForSeconds(0.05f);
-        }
-        _renderer.color = _oldColor;
-    }
+    //IEnumerator BlinkEffect()
+    //{
+    //    _renderer.color = new Color(255, _oldColor.g/2, _oldColor.b/2, 0.8f);
+    //    for (int i = 0; i < 5; i++)
+    //    {
+    //        _renderer.enabled = false;
+    //        yield return new WaitForSeconds(0.1f);
+    //        _renderer.enabled = true;
+    //        yield return new WaitForSeconds(0.05f);
+    //    }
+    //    _renderer.color = _oldColor;
+    //}
 
 }

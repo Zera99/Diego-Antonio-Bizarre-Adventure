3870a56a130ca0da73a852539efcf8b2b72c78d2
Zera99 <razprog@hotmail.com>
2020-06-25 19:50:29 -0300

    Rata con timer y efecto

--- Assets/Scripts/Entities/RatBoi.cs
+++ Assets/Scripts/Entities/RatBoi.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class RatBoi : MonoBehaviour {
     public GameObject hpPrefab;
-    public GameObject effect;
+    public GameObject deathEffect;
     public Transform itemSpawnPoint;
+    public Transform effectSpawnPoint;
     public ShootyHyenaBro hyena;
+    public float timeToDestroy;
+    float _currentTime;
     Animator _anim;
     bool _hasLanded;
+    SpriteRenderer _renderer;
 
     private void Awake() {
         _anim = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        _currentTime = 0;
     }
 
     private void Update() {
-        if(!_hasLanded)
+        if(!_hasLanded) {
             transform.Rotate(0, 0, 20);
+        }
+        _currentTime += Time.deltaTime;
+        if(_currentTime > timeToDestroy) {
+            StartCoroutine(StunCoroutine());
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -39,6 +54,22 @@ public class RatBoi : MonoBehaviour {
     public void RatboiSpawnItem() {
         GameObject hp = Instantiate(hpPrefab);
         hp.transform.position = itemSpawnPoint.position;
+    }
+
+    public void Die() {
+        GameObject effect = Instantiate(deathEffect, effectSpawnPoint);
+        effect.transform.localPosition = Vector3.zero;
+        Destroy(this.gameObject, 0.75f);
+    }
+
+    IEnumerator StunCoroutine() {
+        for (int i = 0; i < 8; i++) {
+            _renderer.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+            _renderer.enabled = true;
+            yield return new WaitForSeconds(0.05f);
+        }
+        Die();
     }
 
 }

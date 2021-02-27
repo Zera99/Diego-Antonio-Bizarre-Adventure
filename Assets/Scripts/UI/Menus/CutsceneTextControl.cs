364a59ae7bc748263be8acb876dc701274724e57
Zera99 <razprog@hotmail.com>
2021-02-27 16:21:45 -0300

    Control de dialogo!

---
+++ Assets/Scripts/UI/Menus/CutsceneTextControl.cs
@@ -0,0 +1,52 @@
+﻿using RedBlueGames.Tools.TextTyper;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneTextControl : MonoBehaviour {
+
+    public List<string> TextToDisplay;
+    public TextTyper typer;
+    public float InitialCutsceneDelay;
+    public float DelayBetweenLetters;
+    int currentIndex;
+
+    private void Awake() {
+        currentIndex = 0;
+    }
+
+    private void Start() {
+        StartCoroutine(RunCutscene());
+    }
+
+    public void StartWaitForInput() {
+        StartCoroutine(WaitForInput());
+    }
+
+    IEnumerator RunCutscene() {
+        if(currentIndex == 0)
+            yield return new WaitForSeconds(InitialCutsceneDelay);
+
+        if (currentIndex < TextToDisplay.Count) {
+            typer.TypeText(TextToDisplay[currentIndex], DelayBetweenLetters);
+            currentIndex++;
+        } else {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    IEnumerator WaitForInput() {
+        Debug.Log("Started Wait for input");
+        yield return new WaitUntil(() => Input.anyKeyDown);
+        //bool notSkipping = true;
+        //while (notSkipping) {
+        //    if (Input.anyKeyDown) {
+        //        notSkipping = false;
+        //    }
+        //    yield return new WaitForEndOfFrame();
+        //}
+        StartCoroutine(RunCutscene());
+    }
+
+}

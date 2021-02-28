28ff53a5ce991f6fb956596fcaded2059c118cc2
Zera99 <razprog@hotmail.com>
2021-02-28 10:44:04 -0300

    Granja 1 y 2

---
+++ Assets/Scripts/World/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        PlayerModel p = collision.gameObject.GetComponent<PlayerModel>();
+        if (p != null)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}

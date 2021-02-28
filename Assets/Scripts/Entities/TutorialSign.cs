e5f95823aaab36429b905a2915aacbd26f97d520
Zera99 <razprog@hotmail.com>
2021-02-28 11:00:39 -0300

    Tutorial Signs

---
+++ Assets/Scripts/Entities/TutorialSign.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSign : MonoBehaviour {
+    private float _startingY;
+    public float amplitude;
+    public float sineSpeed;
+
+    void Float(float startingY, float amp) {
+        transform.position = new Vector3(transform.position.x,
+                                    startingY + (Mathf.Sin(Time.time * sineSpeed) * amp), 0);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        _startingY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        Float(_startingY, amplitude);
+    }
+}

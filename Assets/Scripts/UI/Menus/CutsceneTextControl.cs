b930561eb6b592723a269b984b5a68f89e6b4a8c
Zera99 <razprog@hotmail.com>
2021-03-02 18:30:42 -0300

    Cutscenes Template

--- Assets/Scripts/UI/Menus/CutsceneTextControl.cs
+++ Assets/Scripts/UI/Menus/CutsceneTextControl.cs
@@ -11,9 +11,19 @@ public class CutsceneTextControl : MonoBehaviour {
     public float InitialCutsceneDelay;
     public float DelayBetweenLetters;
     int currentIndex;
+    int newIndex;
+    public List<AudioClip> DiegoSounds;
+    public List<AudioClip> OtaconSounds;
+    public AudioClip RadioSound;
 
+    public Animator DiegoAnimator;
+    public Animator OtaconAnimator;
+    public Animator RadioAnimator;
+
+    AudioSource audioSource;
     private void Awake() {
         currentIndex = 0;
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Start() {
@@ -25,28 +35,50 @@ public class CutsceneTextControl : MonoBehaviour {
     }
 
     IEnumerator RunCutscene() {
-        if(currentIndex == 0)
-            yield return new WaitForSeconds(InitialCutsceneDelay);
+        if (currentIndex == 0) {
+            audioSource.PlayOneShot(RadioSound);
+            //yield return new WaitForSeconds(RadioSound.length + 0.5f);
+            yield return new WaitForSeconds(1.0f);
+        }
 
         if (currentIndex < TextToDisplay.Count) {
             typer.TypeText(TextToDisplay[currentIndex], DelayBetweenLetters);
             currentIndex++;
+            newIndex = currentIndex % 2;
+
+            if (newIndex == 0) {
+                DiegoAnimator.ResetTrigger("Idle");
+                DiegoAnimator.SetTrigger("Talk");
+
+            } else {
+                OtaconAnimator.ResetTrigger("Idle");
+                OtaconAnimator.SetTrigger("Talk");
+            }
+
         } else {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
     IEnumerator WaitForInput() {
         Debug.Log("Started Wait for input");
+        DiegoAnimator.SetTrigger("Idle");
+        DiegoAnimator.ResetTrigger("Talk");
+        OtaconAnimator.SetTrigger("Idle");
+        OtaconAnimator.ResetTrigger("Talk");
         yield return new WaitUntil(() => Input.anyKeyDown);
-        //bool notSkipping = true;
-        //while (notSkipping) {
-        //    if (Input.anyKeyDown) {
-        //        notSkipping = false;
-        //    }
-        //    yield return new WaitForEndOfFrame();
-        //}
         StartCoroutine(RunCutscene());
+    }
+
+    public void PlaySound() {
+
+        if (newIndex == 0) {
+            audioSource.Stop();
+            audioSource.PlayOneShot(DiegoSounds[Random.Range(0, DiegoSounds.Count)]);
+        } else {
+            audioSource.Stop();
+            audioSource.PlayOneShot(OtaconSounds[Random.Range(0, OtaconSounds.Count)]);
+        }
     }
 
 }

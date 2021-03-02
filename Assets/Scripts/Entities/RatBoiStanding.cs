645d5fa408e1bcebe1b6baaff984eb3cdce5745f
Zera99 <razprog@hotmail.com>
2021-03-02 18:46:55 -0300

    RatSlug Standing

---
+++ Assets/Scripts/Entities/RatBoiStanding.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatBoiStanding : MonoBehaviour {
+    public GameObject hpPrefab;
+    public GameObject deathEffect;
+    public Transform itemSpawnPoint;
+    public Transform effectSpawnPoint;
+    public AudioSource audSource;
+    public AudioClip triggerClip;
+    Animator _anim;
+
+    private void Awake() {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.GetComponent<PlayerModel>() != null) {
+            Destroy(GetComponent<BoxCollider2D>());
+            _anim.SetTrigger("spawnItem");
+            Destroy(GetComponent<Rigidbody2D>());
+
+            audSource.clip = triggerClip;
+            audSource.Play();
+
+            Die();
+        }
+
+    }
+
+    public void RatboiSpawnItem() {
+        GameObject hp = Instantiate(hpPrefab);
+        hp.transform.position = itemSpawnPoint.position;
+    }
+
+    public void Die() {
+        GameObject effect = Instantiate(deathEffect, effectSpawnPoint);
+        effect.transform.localPosition = Vector3.zero;
+        Destroy(this.gameObject, 0.75f);
+    }
+}
--- Assets/Scripts/Utility/OtaconCutsceneController.cs
+++ Assets/Scripts/Utility/OtaconCutsceneController.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OtaconCutsceneController : MonoBehaviour {
-    public List<AudioClip> DiegoSounds;
-    public List<AudioClip> OtaconSounds;
+
 
     // Start is called before the first frame update
     void Start() {
